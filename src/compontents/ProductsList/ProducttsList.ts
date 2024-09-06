be3c6276d53f17b29d875527f588cf6ff3a8c28e
sgkochar <sgkochar@gmail.com>
2024-09-06 13:56:35 +0300

    create Catalog page

--- src/compontents/ProductsList/ProducttsList.ts
+++ src/compontents/ProductsList/ProducttsList.ts
@@ -38,6 +38,71 @@ const ProductsList:TProduct[] = [
         brand: "MM's  Brand",
         price: "119.00"
     },
+
+    {
+        id: 4,
+        image: "/images/catalog/5.png",
+        title: "White T-shirt",
+        brand: "Helen's  Brand",
+        price: "11.00"
+    },
+
+    {
+        id: 5,
+        image: "/images/catalog/6.png",
+        title: "Leaves Pattern White...",
+        brand: "paypal's  Brand",
+        price: "77.00"
+    },
+
+    {
+        id: 6,
+        image: "/images/catalog/7.png",
+        title: "White Graphic Crop Top",
+        brand: "woden's  Brand",
+        price: "29.00"
+    },
+
+    {
+        id: 7,
+        image: "/images/catalog/8.png",
+        title: "Barboreal Gray Sweats...",
+        brand: "Priya's  Brand",
+        price: "77.00"
+    },
+    
+    {
+        id: 8,
+        image: "/images/catalog/9.png",
+        title: "Yellow Sweatshirt",
+        brand: "woden's  Brand",
+        price: "29.00"
+    },
+
+    {
+        id: 9,
+        image: "/images/catalog/10.png",
+        title: "Flower Pattern Black C...",
+        brand: "MM's  Brand",
+        price: "37.00"
+    },
+
+    {
+        id: 10,
+        image: "/images/catalog/11.png",
+        title: "I Don't Graphic T-shirt",
+        brand: "Nisargi's  Brand",
+        price: "77.00"
+    },
+
+    {
+        id: 11,
+        image: "/images/catalog/12.png",
+        title: "Blue Flower Print Crop ....",
+        brand: "Mellon's  Brand",
+        price: "37.00"
+    },
+
 ] 
 
 export default ProductsList
--- src/pages/Catalog.tsx
+++ src/pages/Catalog.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 import './css/Catalog.css'
 import Box from '@mui/material/Box';
 import Slider from '@mui/material/Slider';
+import ProductsList from '../compontents/ProductsList/ProducttsList';
+import ProductsItem from '../compontents/ProductsItem/ProductsItem';
 
 function valuetext(value: number) {
     return `$${value}`;
@@ -16,8 +18,8 @@ const Catalog = () => {
     };
     return (
         <>
-            <div className="catalog-wr d-f">
-                <div className="catalog-filter">
+            <div className="catalog-wr d-f jc-sb">
+                <aside className="catalog-filter">
                     <div className="catalog-filter-caption d-f jc-sb">
                         <h3 className="catalog-filter-title">Filter</h3>
                         <div><img src="/images/filter.png" alt="filter" /></div>
@@ -175,8 +177,125 @@ const Catalog = () => {
                             <button className='btn-clear d-b'><img src="/images/cat-icon.png" alt="cat-icon" /></button>
                         </div>
                     </div>
-                </div>
+                </aside>
+                <main className='catalog-main'>
+                    <div className="d-f jc-sb">
+                        <h2 className="catalog-title">Women's Clothing</h2>
+                        <div className="catalog-tabs d-f">
+                            <button className='catalog-tabs-btn tabs-btn__active'>New</button>
+                            <button className='catalog-tabs-btn'>Recommended</button>
+                        </div>
+                    </div>
+                    <div className="catalog-box d-f jc-sb">
+                        {
+                            ProductsList.map((product) => {
+                                return (
+                                    <ProductsItem
+                                        id={product.id}
+                                        image={product.image}
+                                        title={product.title}
+                                        brand={product.brand}
+                                        price={product.price}
+                                    />
+                                )
+                            })
+                        }
+                    </div>
+                </main>
             </div>
+            <section className='catalog-info'>
+                <h3 className="home-bl-title">Clothing for Women Online in India</h3>
+                <h4 className="catalog-info-subtitle">Reexplore Women's Clothing Collection Online at Euphoria</h4>
+                <p className="catalog-info-desc">
+                    Women's Clothing - Are you searching for the best website to buy Clothing for Women online
+                    in India? Well, your search for the coolest and most stylish womens clothing ends here.
+                    From trendy Casual Womens Wear Online shopping to premium quality cotton women's apparel,
+                    <span>Euphoria</span> has closet of Women Collection covered with the latest and best designs of Women's
+                    Clothing Online.
+                </p>
+                <p className="catalog-info-desc" style={{ marginTop: "20px" }}>
+                    Our collection of clothes for women will make you the trendsetter with an iconic resemblance
+                    of choice in Womens Wear.
+                </p>
+                <h4 className="catalog-info-subtitle" style={{ marginTop: "25px" }}>
+                    One-Stop Destination to Shop Every Clothing for Women: Euphoria
+                </h4>
+                <p className="catalog-info-desc">
+                    Today, Clothing for Women is gaining more popularity above all. This is because gone
+                    are the days when women were used to carrying uncomfortable fashion. Today, a lady
+                    looks prettier when she is in Casual Womens Wear which is a comfortable outfit.
+                    Concerning this, <span>Euphoria</span> has a big fat range of Stylish Women's Clothing that
+                    would make her the winner wherever she goes.
+                </p>
+                <p className="catalog-info-desc" style={{ marginTop: "20px" }}>
+                    Our collection of clothes for women will make you the trendsetter with an iconic
+                    resemblance of choice in Womens Wear. It is quite evident to say that there are very
+                    few Womens Clothing online stores where you can buy Western Wear for Women comprising
+                    the premium material and elegant design that you are always seeking for. Basically,
+                </p>
+                <button className="catalog-info-more">See more</button>
+            </section>
+            <section className='catalog-best'>
+                <h2 className="home-bl-title">Buy Women's Clothing at Best Price</h2>
+                <table className='catalog-best-tb'>
+                    <thead>
+                        <tr>
+                            <th>Women's Clothing</th>
+                            <th>Best Price</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>Pick Any 4- Womens Plain T-shirt Combo</td>
+                            <td>₹1099</td>
+                        </tr>
+                        <tr>
+                            <td>Pick Any 4- Plain Womens Boxer Combo</td>
+                            <td>₹1099</td>
+                        </tr>
+                        <tr>
+                            <td>Pick Any 4 - Women Plain Full Sleeve T-shirt Combo</td>
+                            <td>₹1399</td>
+                        </tr>
+                        <tr>
+                            <td>Multicolor Checkered Long Casual Shirts for Women</td>
+                            <td>₹499</td>
+                        </tr>
+                        <tr>
+                            <td>Pick Any 2: Plain Boxy Casual Shirts for Women Combo</td>
+                            <td>₹799</td>
+                        </tr>
+                        <tr>
+                            <td>Blue Floral Anarkali Kurti</td>
+                            <td>₹599</td>
+                        </tr>
+                        <tr>
+                            <td>Jade Black Narrow Cut Flexible Women Jeggings</td>
+                            <td>₹998</td>
+                        </tr>
+                        <tr>
+                            <td>Mustard-yellow Solid Straight-Fit Women Pant</td>
+                            <td>₹499</td>
+                        </tr>
+                        <tr>
+                            <td>Women Pants Combo - Pick Any 2</td>
+                            <td>₹800</td>
+                        </tr>
+                        <tr>
+                            <td>Pista Green Solid Boxy Casual Shirts for Women</td>
+                            <td>₹449</td>
+                        </tr>
+                        <tr>
+                            <td>Plain Burgundy Womens Boxer</td>
+                            <td>₹349</td>
+                        </tr>
+                        <tr>
+                            <td>Striped Front Tie Casual Shirts for Women</td>
+                            <td>₹449</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </section>
         </>
     )
 }
